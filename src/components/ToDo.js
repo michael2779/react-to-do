c7b0138d148e0876ac82955e2fcc6903a42f3571
Michael Yosowitz <michaelyosowitz@gmail.com>
2017-11-21 10:23:27 -0800

    first commit

---
+++ src/components/ToDo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+  render () {
+    return (
+      <li>
+        <input type="checkbox" checked ={this.props.isCompleted} onChange={this.props.toggleComplete }/>
+        <input type="checkbox" onClick={this.props.pleaseDelete } />
+        <span>{ this.props.description}</span>
+      </li>
+    );
+
+  }
+}
+
+export default ToDo;
